3e0d5cc753bf009246c564811301475bade2db8d
James Holman <jameswarringtonholman@gmail.com>
2020-11-14 18:11:53 +1100

    feat: no junmp while fall

--- src/systems/sneatling_system.rs
+++ src/systems/sneatling_system.rs
@@ -23,22 +23,27 @@ impl<'s> System<'s> for SneatlingMovementSystem {
     fn run(&mut self, (mut velocities, sneatlings, input): Self::SystemData) {
         for (_sneatling, velocity) in (&sneatlings, &mut velocities).join() {
             let movement = input.axis_value("player_1_walk");
-            let jump = input.action_is_down("player_1_jump").unwrap_or(false);
-            if jump {
-                velocity.y = 3.;
-            }
-            if velocity.y != 0.0 {
-                return;
-            }
-            if let Some(mv_amount) = movement {
-                if mv_amount != 0.0 {
-                    let scaled_movement = SNEATLING_SPEED * mv_amount;
-                    let new_velocity = velocity.x + scaled_movement;
-                    println!("scaled: {} \n new: {}", scaled_movement, new_velocity);
-                    velocity.x = match new_velocity.abs() > (SNEATLING_SPEED * scaled_movement).abs() {
-                        true => scaled_movement,
-                        false =>  new_velocity,
-                    };
+
+            /* Actions where the Sneatling can be falling */
+
+            if velocity.on_floor {
+                /* Actions Where the Sneatling is on the floor*/
+                let jump = input.action_is_down("player_1_jump").unwrap_or(false);
+                if jump {
+                    velocity.y = 3.;
+                }
+
+                if let Some(mv_amount) = movement {
+                    if mv_amount != 0.0 {
+                        let scaled_movement = SNEATLING_SPEED * mv_amount;
+                        let new_velocity = velocity.x + scaled_movement;
+                        println!("scaled: {} \n new: {}", scaled_movement, new_velocity);
+                        velocity.x =
+                            match new_velocity.abs() > (SNEATLING_SPEED * scaled_movement).abs() {
+                                true => scaled_movement,
+                                false => new_velocity,
+                            };
+                    }
                 }
             }
         }
