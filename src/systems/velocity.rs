69e3fc35fc143f00c1d7fb4975165c0de5c6c3d6
James Holman <jameswarringtonholman@gmail.com>
2020-11-14 18:08:31 +1100

    fix: falling through floor

--- src/systems/velocity.rs
+++ src/systems/velocity.rs
@@ -24,9 +24,6 @@ impl<'s> System<'s> for VelocitySystem {
 
     fn run(&mut self, (mut velocities, mut transforms, time): Self::SystemData) {
         for (mut velocity, transform) in (&mut velocities, &mut transforms).join() {
-            if velocity.y < MAX_Y_SPEED && velocity.on_floor == false {
-                velocity.y = (velocity.y + GRAVITY * time.delta_seconds()).min(MAX_Y_SPEED);
-            }
 
             /* Updating transforms from velocities */
             if velocity.x != 0.0 {
@@ -37,15 +34,22 @@ impl<'s> System<'s> for VelocitySystem {
                 transform.prepend_translation_y(velocity.y);
             };
 
+            if velocity.y < MAX_Y_SPEED && velocity.on_floor == false {
+                velocity.y = (velocity.y + GRAVITY * time.delta_seconds()).min(MAX_Y_SPEED);
+            }
+
             /* Updating velocities */
             if !velocity.on_floor {
+                println!("NOT ON FLOOR");
                 if velocity.x > 0.0 {
                     velocity.x = (velocity.x - time.delta_seconds() * X_AIR_DRAG).min(0.0);
                 }
                 if velocity.x < 0.0 {
                     velocity.x = (velocity.x + time.delta_seconds() * X_AIR_DRAG).max(0.0);
                 }
-            } else {
+            }  
+            if velocity.on_floor {
+                println!("ON FLOOR");
                 velocity.x = 0.0;
                 velocity.y = 0.0;
             }
