c0a9b566f5976e99acbbb9052b3084100de20489
James Holman <jameswarringtonholman@gmail.com>
2020-11-14 16:13:35 +1100

    feat: velocity

--- src/systems/velocity.rs
+++ src/systems/velocity.rs
@@ -1,5 +1,5 @@
 use amethyst::{
-    core::{SystemDesc, Transform},
+    core::{SystemDesc, Transform, timing::Time},
     derive::SystemDesc,
     ecs::{Join, Read, ReadStorage, System, SystemData, World, WriteStorage},
     input::{InputHandler, StringBindings},
@@ -10,14 +10,17 @@ pub use crate::components::Velocity;
 #[derive(SystemDesc)]
 pub struct VelocitySystem;
 
+const X_DRAG: f32 = 2.;
+
 impl<'s> System<'s> for VelocitySystem {
     type SystemData = (
         WriteStorage<'s, Velocity>,
         WriteStorage<'s, Transform>,
+        Read<'s, Time>
     );
 
-    fn run(&mut self, (velocities, mut transforms): Self::SystemData) {
-        for (velocity, transform) in (&velocities, &mut transforms).join() {
+    fn run(&mut self, (mut velocities, mut transforms, time): Self::SystemData) {
+        for (mut velocity, transform) in (&mut velocities, &mut transforms).join() {
             if velocity.x != 0.0 {
                 println!("moving by {}", velocity.x);
                 transform.prepend_translation_x(velocity.x);
@@ -25,6 +28,12 @@ impl<'s> System<'s> for VelocitySystem {
             if velocity.y != 0.0 {
                 transform.prepend_translation_y(velocity.y);
             };
+            if velocity.x > 0.0 {
+                velocity.x = (velocity.x - time.delta_seconds() * X_DRAG).min(0.0);
+            }
+            if velocity.x < 0.0 {
+                velocity.x = (velocity.x + time.delta_seconds() * X_DRAG).max(0.0);
+            }
         }
     }
 }
