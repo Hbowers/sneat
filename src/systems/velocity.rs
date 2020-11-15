0b66058e9f4e557620a00dbc31c964c922fa4de2
James H <32926722+jayy-lmao@users.noreply.github.com>
2020-11-15 20:22:34 +1100

    feat/wall collision (#13)
    * :sparkles: we can now define maps in a ron
    
    * feat: wall collision

--- src/systems/velocity.rs
+++ src/systems/velocity.rs
@@ -14,6 +14,7 @@ const X_DRAG: f32 = 9.;
 const MAX_Y_SPEED: f32 = 0.7;
 // FIXME: Change back to a meaningful value
 const GRAVITY: f32 = -1.;
+const BOUNCEBACK: f32 = 0.5;
 
 impl<'s> System<'s> for VelocitySystem {
     type SystemData = (
@@ -24,22 +25,11 @@ impl<'s> System<'s> for VelocitySystem {
 
     fn run(&mut self, (mut velocities, mut transforms, time): Self::SystemData) {
         for (mut velocity, transform) in (&mut velocities, &mut transforms).join() {
-            /* Updating transforms from velocities */
-            if velocity.x != 0.0 {
-                transform.prepend_translation_x(velocity.x);
-            };
-            if velocity.y != 0.0 {
-                transform.prepend_translation_y(velocity.y);
-            };
-
-            if !velocity.collided {
+            /* Updating velocities */
+            if !velocity.collided_y {
                 velocity.y = (velocity.y + GRAVITY * time.delta_seconds())
                     .min(MAX_Y_SPEED)
                     .max(-MAX_Y_SPEED);
-            }
-
-            /* Updating velocities */
-            if !velocity.collided {
                 if velocity.x >= 0.0 {
                     velocity.x = (velocity.x - time.delta_seconds() * X_AIR_DRAG).max(0.);
                 }
@@ -47,7 +37,7 @@ impl<'s> System<'s> for VelocitySystem {
                     velocity.x = (velocity.x + time.delta_seconds() * X_AIR_DRAG).min(0.);
                 }
             }
-            if velocity.collided {
+            if velocity.collided_y {
                 velocity.y = 0.0;
                 if velocity.x >= 0.0 {
                     velocity.x = (velocity.x - time.delta_seconds() * X_DRAG).max(0.);
@@ -56,6 +46,21 @@ impl<'s> System<'s> for VelocitySystem {
                     velocity.x = (velocity.x + time.delta_seconds() * X_DRAG).min(0.);
                 }
             }
+
+            if velocity.collided_x_left {
+               velocity.x = -BOUNCEBACK;
+            };
+            if velocity.collided_x_right {
+               velocity.x = BOUNCEBACK;
+            };
+
+            /* Updating transforms from velocities */
+            if velocity.x != 0.0 {
+                transform.prepend_translation_x(velocity.x);
+            };
+            if velocity.y != 0.0 {
+                transform.prepend_translation_y(velocity.y);
+            };
         }
     }
 }
